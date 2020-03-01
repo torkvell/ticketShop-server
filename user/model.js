770853e7656299c1bead7764e240e5357b4d7704
torkvell <tor.kvell@outlook.com>
2020-03-01 17:25:00 +0100

    new api endpoints --> rein feedback

--- user/model.js
+++ user/model.js
@@ -3,33 +3,25 @@ const sequelize = require("../db");
 const Comment = require("../comment/model");
 const Event = require("../event/model");
 
-// sync model to database
-const User = sequelize.define(
-  "user",
-  {
-    firstName: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    lastName: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    email: {
-      type: Sequelize.STRING,
-      unique: true,
-      allowNull: false
-    },
-    password: {
-      type: Sequelize.STRING,
-      allowNull: false
-    }
+const User = sequelize.define("user", {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false
   }
-  // {
-  //   timestamps: false,
-  //   tableName: "users"
-  // }
-);
+});
 User.hasMany(Event);
 User.hasMany(Comment);
 module.exports = User;
